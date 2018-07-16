4ebae18f4401a527ebe7b78567bbc595807adb8a
Araba <araba.taylor20@gmail.com>
2018-07-16 11:26:43 -0400

    Trip#new

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,23 @@
 class TripsController < ApplicationController
+
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    if @trip.save
+
+  end
+
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(
+      :name
+      :start_date
+      :end_date
+    )
+
+  end
 end
